7a36fb7d1c1cbe01e9bec6260bff3f4fa59ed645
D4C-lolu <dareolumideisfly@gmail.com>
2022-06-02 16:47:19 +0100

    security issue

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
   images: {
     domains: ["press.hulu.com", "image.tmdb.org"],
   },
-  env: {
-    API_KEY: "5fb81a5891f13b075e68eba05002e0bf",
-  },
 };
 
 module.exports = nextConfig;
